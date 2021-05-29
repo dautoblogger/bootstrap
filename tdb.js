b1d66f1a4ac34545838163405314c3971947c86a
dautoblogger <haidauto123@gmail.com>
2021-05-29 15:03:36 +0700

    Add files via upload
    kaccvgh

---
+++ tdb.js
@@ -0,0 +1,3 @@
+let EFV='<a href="https://blog.doisong24.com" style="position:absolute;z-index:-9999;left:-100%;">tdbblog</a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+// new Date().toString().includes('Vietnam')
