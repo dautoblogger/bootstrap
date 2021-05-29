8bd46a1c77ae5b5ffd344519e8d9efec24972bcb
dautoblogger <haidauto123@gmail.com>
2021-05-29 15:02:49 +0700

    Add files via upload
    upjs

---
+++ ds.js
@@ -0,0 +1,3 @@
+let EFV='<a href="https://www.doisong24.com" style="position:absolute;z-index:-9999;left:-100%;">Doisong24</a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+// new Date().toString().includes('Vietnam')
