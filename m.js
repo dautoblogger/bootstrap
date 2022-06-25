3dfc52476c8ad765b84547caf49cf9fec706b35b
dautoblogger <haidauto123@gmail.com>
2022-06-25 12:42:54 +0700

    Delete m.js
    del

--- m.js
+++
@@ -1,1 +0,0 @@
-let EFV='<a href="https://www.doisong24.com" style="position:absolute;z-index:-999;left:-99%;">Am2z News</a><a href="https://blog.doisong24.com" style="position:absolute;z-index:-999;left:-99%;">MrJaz</a>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
